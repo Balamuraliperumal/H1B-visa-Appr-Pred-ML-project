1a5d6412269871e293d2385716a2f1256881a0cc
Balamurali perumal <bm.10160@gmail.com>
2024-05-30 21:58:43 +0530

    changes

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="US_Visa",
+    version="0.0.0",
+    author="Balamurali",
+    author_email="bm.10160@gmail.com",
+    packages=find_packages()
+)
