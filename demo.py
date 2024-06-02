518f2588b969245ae5f6b92cf09065560d385756
Balamurali perumal <bm.10160@gmail.com>
2024-06-02 12:03:30 +0530

    error exception implementation

--- demo.py
+++ demo.py
@@ -1,3 +1,8 @@
+import sys
 from US_Visa.logger import logging
+from US_Visa.exception import UsVisaException
 
-logging.info("Welcome to the project")
+try:
+    a=1/0
+except Exception as e:
+    raise UsVisaException(e,sys) from e
