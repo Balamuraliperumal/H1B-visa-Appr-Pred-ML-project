780fce940cc7ec488e356a8c1064a8d8efe084c2
Balamurali perumal <bm.10160@gmail.com>
2024-06-22 16:59:36 +0530

    model trainer

--- US_Visa/entity/estimator.py
+++ US_Visa/entity/estimator.py
@@ -15,3 +15,34 @@ class TargetValueMapping:
     def reverse_mapping(self):
         mapping_response = self._asdict()
         return dict(zip(mapping_response.values(), mapping_response.keys()))
+
+
+class projectmodel:
+
+    def __init__(self,preprocessing_object:Pipeline,trained_model_object: object):
+
+        self.preprocessing_object = preprocessing_object
+        self.trained_model_object = trained_model_object
+    
+
+    def predict(self,df : DataFrame) -> DataFrame:
+
+        logging.info("Entered predict method of Trained Model class")
+
+        try:
+            logging.info("Using the trained model to get predictions")
+
+            transformed_feature = self.preprocessing_object.transform(df)
+
+            logging.info("Used the trained model to get predictions")
+
+            return self.trained_model_object.predict(transformed_feature)
+
+        except Exception as e:
+            raise UsVisaException(e, sys) from e
+    
+    def __repr__(self):
+        return f"{type(self.trained_model_object).__name__}()"
+    
+    def __str__(self):
+        return f"{type(self.trained_model_object).__name__}()"
--- US_Visa/pipeline/training_pipeline.py
+++ US_Visa/pipeline/training_pipeline.py
@@ -7,15 +7,18 @@ from US_Visa.exception import UsVisaException
 from US_Visa.components.data_ingestion import DataIngestion
 from US_Visa.components.data_validation import DataValidation
 from US_Visa.components.data_transformation import DataTransformation
+from US_Visa.components.model_trainer import ModelTrainer
 
 
 from US_Visa.entity.config_entity import (DataIngestionConfig,
                                           DataValidationConfig,
-                                          DataTransformationConfig)
+                                          DataTransformationConfig,
+                                          ModelTrainerConfig)
 
 from US_Visa.entity.artifact_entity import (DataIngestionArtifact,
                                             DataValidationArtifact,
-                                            DataTransformationArtifact)
+                                            DataTransformationArtifact,
+                                            ModelTrainerArtifact)
 
 
 class TrainPipeline:
@@ -24,6 +27,7 @@ class TrainPipeline:
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
         self.data_transformation_config = DataTransformationConfig()
+        self.model_trainer_config = ModelTrainerConfig()
 
 
     def start_data_ingestion(self) -> DataIngestionArtifact:
@@ -71,6 +75,19 @@ class TrainPipeline:
             return data_transformation_artifact
         except Exception as e:
             raise UsVisaException(e, sys)
+        
+
+    def start_model_trainer(self, data_transformation_artifact: DataTransformationArtifact) -> ModelTrainerArtifact:
+
+        try:
+            model_trainer = ModelTrainer(data_transformation_artifact=data_transformation_artifact,
+                                        model_trainer_config = self.model_trainer_config
+                                        )
+            
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
+        except Exception as e:
+            raise UsVisaException(e, sys) from e
 
     def run_pipeline(self) -> None:
 
@@ -78,6 +95,7 @@ class TrainPipeline:
             data_ingestion_artifact = self.start_data_ingestion()
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
             data_transformation_artifact = self.start_data_transformation(data_ingestion_artifact=data_ingestion_artifact,data_validation_artifact=data_validation_artifact)
+            model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
 
         except Exception as e:
             raise UsVisaException(e, sys) from e
